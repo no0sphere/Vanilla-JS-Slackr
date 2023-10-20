623182e013abb04c44815733fa963d137e7bd13e
Hao Zeng <z5445039@ad.unsw.edu.au>
2023-10-20 16:10:32 +1100

    2.1

--- frontend/src/helpers.js
+++ frontend/src/helpers.js
@@ -29,3 +29,23 @@ export function fileToDataUrl(file) {
     reader.readAsDataURL(file);
     return dataUrlPromise;
 }
+
+export const apiCallPost = (path, body) => {
+  return new Promise((resolve, reject) => {
+      fetch('http://localhost:5005/' + path, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+          },
+        body: JSON.stringify(body)
+      })
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.error) {
+              alert(data.error);
+            } else {
+              resolve(data);
+            }
+          });
+  });
+};
